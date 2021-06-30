99b28da11f13b422c790edfa0d1d9c57807d6cb5
grumbach <anselmega@gmail.com>
2021-06-30 13:58:39 +0200

    fix: replace deprecated bench_function_over_inputs with BenchmarkGroup

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,6 +1,6 @@
 use blsttc::poly::Poly;
 use blsttc::Fr;
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use ff::Field;
 
 const TEST_DEGREES: [usize; 4] = [5, 10, 20, 40];
@@ -14,72 +14,89 @@ mod poly_benches {
 
     /// Benchmarks multiplication of two polynomials.
     fn multiplication(c: &mut Criterion) {
+        let mut group = c.benchmark_group("Polynomial multiplication Group");
         let mut rng = XorShiftRng::from_seed(RNG_SEED);
-        c.bench_function_over_inputs(
-            "Polynomial multiplication",
-            move |b, &&deg| {
-                let rand_factors = || {
-                    let lhs = Poly::random(deg, &mut rng);
-                    let rhs = Poly::random(deg, &mut rng);
-                    (lhs, rhs)
-                };
-                b.iter_with_setup(rand_factors, |(lhs, rhs)| &lhs * &rhs)
-            },
-            &TEST_DEGREES,
-        );
+        for deg in TEST_DEGREES {
+            let parameter_string = format!("{}", deg);
+            group.bench_with_input(
+                BenchmarkId::new("Polynomial multiplication", parameter_string),
+                &deg,
+                |b, deg| {
+                    let rand_factors = || {
+                        let lhs = Poly::random(*deg, &mut rng);
+                        let rhs = Poly::random(*deg, &mut rng);
+                        (lhs, rhs)
+                    };
+                    b.iter_with_setup(rand_factors, |(lhs, rhs)| &lhs * &rhs)
+                },
+            );
+        }
+        group.finish();
     }
 
     /// Benchmarks subtraction of two polynomials
     fn subtraction(c: &mut Criterion) {
+        let mut group = c.benchmark_group("Polynomial subtraction");
         let mut rng = XorShiftRng::from_seed(RNG_SEED);
-        c.bench_function_over_inputs(
-            "Polynomial subtraction",
-            move |b, &&deg| {
-                let rand_factors = || {
-                    let lhs = Poly::random(deg, &mut rng);
-                    let rhs = Poly::random(deg, &mut rng);
-                    (lhs, rhs)
-                };
-                b.iter_with_setup(rand_factors, |(lhs, rhs)| &lhs - &rhs)
-            },
-            &TEST_DEGREES,
-        );
+        for deg in TEST_DEGREES {
+            let parameter_string = format!("{}", deg);
+            group.bench_with_input(
+                BenchmarkId::new("Polynomial subtraction", parameter_string),
+                &deg,
+                |b, deg| {
+                    let rand_factors = || {
+                        let lhs = Poly::random(*deg, &mut rng);
+                        let rhs = Poly::random(*deg, &mut rng);
+                        (lhs, rhs)
+                    };
+                    b.iter_with_setup(rand_factors, |(lhs, rhs)| &lhs - &rhs)
+                },
+            );
+        }
     }
 
     /// Benchmarks addition of two polynomials
     fn addition(c: &mut Criterion) {
+        let mut group = c.benchmark_group("Polynomial addition");
         let mut rng = XorShiftRng::from_seed(RNG_SEED);
-        c.bench_function_over_inputs(
-            "Polynomial addition",
-            move |b, &&deg| {
-                let rand_factors = || {
-                    let lhs = Poly::random(deg, &mut rng);
-                    let rhs = Poly::random(deg, &mut rng);
-                    (lhs, rhs)
-                };
-                b.iter_with_setup(rand_factors, |(lhs, rhs)| &lhs + &rhs)
-            },
-            &TEST_DEGREES,
-        );
+        for deg in TEST_DEGREES {
+            let parameter_string = format!("{}", deg);
+            group.bench_with_input(
+                BenchmarkId::new("Polynomial addition", parameter_string),
+                &deg,
+                |b, deg| {
+                    let rand_factors = || {
+                        let lhs = Poly::random(*deg, &mut rng);
+                        let rhs = Poly::random(*deg, &mut rng);
+                        (lhs, rhs)
+                    };
+                    b.iter_with_setup(rand_factors, |(lhs, rhs)| &lhs + &rhs)
+                },
+            );
+        }
     }
 
     /// Benchmarks Lagrange interpolation for a polynomial.
     fn interpolate(c: &mut Criterion) {
+        let mut group = c.benchmark_group("Polynomial interpolation");
         let mut rng = XorShiftRng::from_seed(RNG_SEED);
-        c.bench_function_over_inputs(
-            "Polynomial interpolation",
-            move |b, &&deg| {
-                b.iter_with_setup(
-                    || {
-                        (0..=deg)
-                            .map(|i| (i, Fr::random(&mut rng)))
-                            .collect::<Vec<_>>()
-                    },
-                    Poly::interpolate,
-                )
-            },
-            &TEST_DEGREES,
-        );
+        for deg in TEST_DEGREES {
+            let parameter_string = format!("{}", deg);
+            group.bench_with_input(
+                BenchmarkId::new("Polynomial interpolation", parameter_string),
+                &deg,
+                |b, deg| {
+                    b.iter_with_setup(
+                        || {
+                            (0..=*deg)
+                                .map(|i| (i, Fr::random(&mut rng)))
+                                .collect::<Vec<_>>()
+                        },
+                        Poly::interpolate,
+                    )
+                },
+            );
+        }
     }
 
     criterion_group! {
@@ -99,26 +116,30 @@ mod public_key_set_benches {
     /// Benchmarks combining signatures
     fn combine_signatures(c: &mut Criterion) {
         let mut rng = XorShiftRng::from_seed(RNG_SEED);
+        let mut group = c.benchmark_group("Combine Signatures");
         let msg = "Test message";
-        c.bench_function_over_inputs(
-            "Combine Signatures",
-            move |b, &&threshold| {
-                let sk_set = SecretKeySet::random(threshold, &mut rng);
-                let pk_set = sk_set.public_keys();
-                let sigs: BTreeMap<_, _> = (0..=threshold)
-                    .map(|i| {
-                        let sig = sk_set.secret_key_share(i).sign(msg);
-                        (i, sig)
+        for threshold in TEST_THRESHOLDS {
+            let parameter_string = format!("{}", threshold);
+            group.bench_with_input(
+                BenchmarkId::new("Combine Signatures", parameter_string),
+                &threshold,
+                |b, threshold| {
+                    let sk_set = SecretKeySet::random(*threshold, &mut rng);
+                    let pk_set = sk_set.public_keys();
+                    let sigs: BTreeMap<_, _> = (0..=*threshold)
+                        .map(|i| {
+                            let sig = sk_set.secret_key_share(i).sign(msg);
+                            (i, sig)
+                        })
+                        .collect();
+                    b.iter(|| {
+                        pk_set
+                            .combine_signatures(&sigs)
+                            .expect("could not combine signatures");
                     })
-                    .collect();
-                b.iter(|| {
-                    pk_set
-                        .combine_signatures(&sigs)
-                        .expect("could not combine signatures");
-                })
-            },
-            &TEST_THRESHOLDS,
-        );
+                },
+            );
+        }
     }
 
     criterion_group! {
