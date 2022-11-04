085aab4b0e72b8ec54d7b848254848c30075eb67
davidrusu <davidrusu@users.noreply.github.com>
2022-11-04 17:48:34 +0000

    deploy: fee477804c713f2e4eb9c557e2625ce1622af436

--- implementors/blsttc/into_fr/trait.IntoFr.js
+++ implementors/blsttc/into_fr/trait.IntoFr.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["blsttc"] = [];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"blsttc":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
