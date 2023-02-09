04b2dde21a2099f121ae2b18b9ec12bff09c37f7
davidrusu <davidrusu@users.noreply.github.com>
2023-02-09 16:21:34 +0000

    deploy: 9a84f337b87615776c3758e3dd3c00771a141e09

--- implementors/core/ops/deref/trait.Deref.js
+++ implementors/core/ops/deref/trait.Deref.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"blsttc":[["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.65.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"blsttc/serde_impl/struct.SerdeSecret.html\" title=\"struct blsttc::serde_impl::SerdeSecret\">SerdeSecret</a>&lt;T&gt;"]]
+"blsttc":[["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.1/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"blsttc/serde_impl/struct.SerdeSecret.html\" title=\"struct blsttc::serde_impl::SerdeSecret\">SerdeSecret</a>&lt;T&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
